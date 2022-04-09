f18825b432d6098dedce00376c114836fa7a70c5
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-10 01:51:28 +0530

    calculating wage till no of working days or workoing hrs per mont is reached

--- EmpWage_Practice.java
+++ EmpWage_Practice.java
@@ -3,9 +3,9 @@ package com.OopsConcept;
 public class EmpWage_Practice {
 
 
-        //UC5
+        //UC6
 
-        //calculating wage for month assuming 20 working days in month
+        //CALCULATING WAGE TILL NUMBER OF WORKING DAYS OR TOTAL WORKING HOUR IS REACHED
 
 
 
@@ -14,6 +14,7 @@ public class EmpWage_Practice {
     public static final int IS_PART_TIME = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HRS_IN_MONTH = 10;
 
     public static void main(String[] args) {
 
@@ -23,11 +24,13 @@ public class EmpWage_Practice {
 
         //VARIABLES
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
+        int empWage = 0;;
         int day =0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0 ;
         //computation
-        for (day = 0; day < NUM_OF_WORKING_DAYS; day++){
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case 0://IS_PART_TIME
@@ -41,10 +44,11 @@ public class EmpWage_Practice {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Emp wage:" + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Days#: " + totalWorkingDays + " Emp Hr: " + empHrs);
+
         }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("total emp wage: " + totalEmpWage);
 
 
