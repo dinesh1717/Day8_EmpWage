e5be9aa498caa569cf21e3f3701661751172987e
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-10 16:41:12 +0530

    refactoring the code

--- EmpWage_Practice.java
+++ EmpWage_Practice.java
@@ -3,31 +3,27 @@ package com.OopsConcept;
 public class EmpWage_Practice {
 
 
-        //UC6
+    //UC7
 
-        //CALCULATING WAGE TILL NUMBER OF WORKING DAYS OR TOTAL WORKING HOUR IS REACHED
+    // refactor the cod using class method and compute employee wage
 
 
-
-        //constants
+    //constants
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
     public static final int MAX_HRS_IN_MONTH = 10;
 
-    public static void main(String[] args) {
-
-
-
-
+    public static int computeEmpWage() {
 
         //VARIABLES
         int empHrs = 0;
-        int empWage = 0;;
-        int day =0;
+        int empWage = 0;
+        ;
+        int day = 0;
         int totalWorkingDays = 0;
-        int totalEmpHrs = 0 ;
+        int totalEmpHrs = 0;
         //computation
         while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
@@ -50,12 +46,13 @@ public class EmpWage_Practice {
         }
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("total emp wage: " + totalEmpWage);
-
-
-
-
+        return totalEmpWage;
     }
 
-
+    public static void main(String[] args) {
+        computeEmpWage();
+    }
 
 }
+
+
